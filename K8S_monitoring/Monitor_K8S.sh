0cd5354c8011c46bbed855a48242caac0ef141be
ahugla <ahugla@vmware.com>
2019-09-26 11:55:57 +0200

    Step 2 done

--- K8S_monitoring/Monitor_K8S.sh
+++ K8S_monitoring/Monitor_K8S.sh
@@ -3,28 +3,33 @@ VAR_WAVEFRONT_SERVER="ALEX_CLUSTER.wavefront.com"
 VAR_WAVEFRONT_TOKEN="alextoken"
 
 
+mkdir /root/DeployProxy
+cd /root/DeployProxy
 
 #Step 1. Deploy a Wavefront Proxy in Kubernetes
 #----------------------------------------------
-
-
-
-mkdir /root/DeployProxy
-rm /root/DeployProxy/wavefront.yaml
-cd /root/DeployProxy
+rm -f /root/DeployProxy/wavefront.yaml
 curl -O  https://raw.githubusercontent.com/wavefrontHQ/wavefront-kubernetes/master/wavefront-proxy/wavefront.yaml
-
 sed -i -e 's/YOUR_CLUSTER.wavefront.com/'"$VAR_WAVEFRONT_SERVER"'/g' wavefront.yaml 
 sed -i -e 's/value: YOUR_API_TOKEN/value: '"$VAR_WAVEFRONT_TOKEN"'/g' wavefront.yaml 
-
 
 
 #Step 2. Deploy the kube-state-metrics Service
 #---------------------------------------------
 
+rm -f /root/DeployProxy/kube-state.yaml
+curl -O https://raw.githubusercontent.com/wavefrontHQ/wavefront-kubernetes/master/ksm-all-in-one/kube-state.yaml
 
 
 
 
 #Step 3. Deploy Wavefront Collector for Kubernetes
 #-------------------------------------------------
+
+
+
+
+#Deploy yaml files
+#-----------------
+#kubectl create -f /root/DeployProxy/wavefront.yaml
+#kubectl create -f /root/DeployProxy/kube-state.yaml
