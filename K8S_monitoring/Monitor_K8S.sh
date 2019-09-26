509efaf0b1f3c68a412c93929861da4d46ac872b
ahugla <ahugla@vmware.com>
2019-09-26 11:43:45 +0200

    Update Monitor_K8S.sh

--- K8S_monitoring/Monitor_K8S.sh
+++ K8S_monitoring/Monitor_K8S.sh
@@ -1,5 +1,5 @@
 
-VAR_WAVEFRONT_URL="https://ALEX_CLUSTER.wavefront.com/api/"
+VAR_WAVEFRONT_SERVER="ALEX_CLUSTER.wavefront.com"
 VAR-WAVEFRONT_TOKEN="alextoken"
 
 
@@ -7,12 +7,15 @@ VAR-WAVEFRONT_TOKEN="alextoken"
 #Step 1. Deploy a Wavefront Proxy in Kubernetes
 #----------------------------------------------
 
+
+
 mkdir /root/DeployProxy
+rm /root/DeployProxy/wavefront.yaml
 cd /root/DeployProxy
 curl -O  https://raw.githubusercontent.com/wavefrontHQ/wavefront-kubernetes/master/wavefront-proxy/wavefront.yaml
 
-sed -i -e 's/YOUR_CLUSTER.wavefront.com/'"$VAR_WAVEFRONT_URL"'/g' wavefront.yaml 
-sed -i -e 's/value: YOUR_API_TOKEN/'"$VAR_WAVEFRONT_TOKEN"'/g' wavefront.yaml 
+sed -i -e 's/YOUR_CLUSTER.wavefront.com/'"$VAR_WAVEFRONT_SERVER"'/g' wavefront.yaml 
+sed -i -e 's/value: YOUR_API_TOKEN/value: '"$VAR_WAVEFRONT_TOKEN"'/g' wavefront.yaml 
 
 
 
